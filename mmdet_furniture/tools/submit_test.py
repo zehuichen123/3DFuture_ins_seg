530d436b80d68854ed521113e184f13fa622559f
v-qjqs <qjliqiaofei@gmail.com>
2020-07-30 14:46:49 +0800

    rewrite sumbit_test.py

--- mmdet_furniture/tools/submit_test.py
+++ mmdet_furniture/tools/submit_test.py
@@ -1,46 +1,51 @@
 import mmcv
-
-root='/mnt/truenas/scratch/lqf/code/mmdet_lqf/whole_pkl/test/'
-filepath='pointrend_res2net101_dcn_fpnbfp_large_coarsehead_fp16_larger_img_p2p6_1200_1400_enrichfeat_largeboxalign/'
-filename='120013001400test.pkl'
-pred_data = mmcv.load(root+filepath+filename)
-data_path = '/mnt/truenas/scratch/lqf/code/czh/data/future/'
-val_anno = data_path + 'annotations/test_set.json'  # NOTE
-val_anno = mmcv.load(val_anno)
-
-anno_list = []
-for anno, pred in zip(val_anno['images'], pred_data):
-    bbox_data = pred[0]; mask_data = pred[1]
-    for cid in range(34):
-        bboxes = bbox_data[cid]
-        masks = mask_data[cid]
-        for ii in range(bboxes.shape[0]):
-            x1, y1, x2, y2, score = bboxes[ii]
-            x = (x1 + x2) / 2; y = (y1 + y2) / 2
-            w = x2 - x1 + 1; h = y2 - y1 + 1
-            mask = masks[ii]
-            res = {
-                'image_id': anno['id'],
-                'category_id': cid + 1,
-                'segmentation': {'size': mask['size'], 'counts': mask['counts'].decode('utf-8')},
-                'bbox': [float(x), float(y), float(w), float(h)],
-                'score': float(score),
-            }
-            anno_list.append(res)
+import os.path as osp
 
 
-img_list = []
-for img_info in val_anno['images']:
-    res = {
-        'image_id': img_info['id'],
-        'width': img_info['width'],
-        'height': img_info['height'],
-        'file_name': img_info['file_name']
-    }
-    img_list.append(res)
+def pkl2json(root, config_name):
+    pklpath = osp.join(root, 'out_pkl', config_name, 'segmentation_resutls.pkl')
+    pred_data = mmcv.load(pklpath)
+    anno_path = osp.join(root, 'data/future/annotations', 'test_set.json')
+    test_anno = mmcv.load(anno_path)
 
-submit = {}
-submit['images'] = img_list
-submit['annotations'] = anno_list
-outname='segmentation_resutls_{}'.format(filename.replace('.pkl','.json'))
-mmcv.dump(submit, root+filepath+outname)
+    anno_list = []
+    for anno, pred in zip(test_anno['images'], pred_data):
+        bbox_data = pred[0]; mask_data = pred[1]
+        for cid in range(34):
+            bboxes = bbox_data[cid]
+            masks = mask_data[cid]
+            for ii in range(bboxes.shape[0]):
+                x1, y1, x2, y2, score = bboxes[ii]
+                x = (x1 + x2) / 2; y = (y1 + y2) / 2
+                w = x2 - x1 + 1; h = y2 - y1 + 1
+                mask = masks[ii]
+                res = {
+                    'image_id': anno['id'],
+                    'category_id': cid + 1,
+                    'segmentation': {'size': mask['size'], 'counts': mask['counts'].decode('utf-8')},
+                    'bbox': [float(x), float(y), float(w), float(h)],
+                    'score': float(score),
+                }
+                anno_list.append(res)
+
+    img_list = []
+    for img_info in test_anno['images']:
+        res = {
+            'image_id': img_info['id'],
+            'width': img_info['width'],
+            'height': img_info['height'],
+            'file_name': img_info['file_name']
+        }
+        img_list.append(res)
+
+    submit = {}
+    submit['images'] = img_list
+    submit['annotations'] = anno_list
+    save_path = osp.join(root, 'out_pkl', config_name, 'segmentation_resutls.json')
+    mmcv.dump(submit, save_path)
+
+
+if __name__ == '__main__':
+    root = '/mnt/truenas/scratch/lqf/code/github/3DFuture_ins_seg/mmdet_furniture/'  # absolute path for mmdet_furniture
+    config_name = 'pointrend_x101_64x4d_dcn_fpn_fp16_p2p6'
+    pkl2json(root, config_name)
